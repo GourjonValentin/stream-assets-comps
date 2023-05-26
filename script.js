850ab5fda755b348345a06726268c1b7794a745f
Nojruog <valentin.gourjon@gmail.com>
2023-05-26 17:54:31 +0200

    Create initial form

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script permettant de traiter les données de l'ordre de passage
+// et de les afficher dans le tableau de l'ordre de passage
+// Auteur: Valentin Gourjon
+
+// Fonction permettant de récupérer les données de l'ordre de passage
+// et de les afficher dans le tableau de l'ordre de passage
+
+function getFile() {
+
+}
